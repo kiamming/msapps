597bf8a2193a8700fe3a2915a1fef327b1da452d
Jason Chui <kiam_ming@yahoo.com>
2013-09-30 00:11:42 +0800

    Fastclick corrected

--- public/js/states/sample_states.js
+++ public/js/states/sample_states.js
@@ -36,12 +36,7 @@ angular.module('myApp')
             // Use a url of "/" to set a state as the "index".
             url: "/",
 
-            // Example of an inline template string. By default, templates
-            // will populate the ui-view within the parent state's template.
-            // For top level states, like this one, the parent template is
-            // the index.html file. So this template will be inserted into the
-            // ui-view within index.html.
-            templateUrl: '../partials/home.html'
+            //templateUrl: '../partials/home.html'
           })
 
             //////////////////////////////////////////////////////////////
